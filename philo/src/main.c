b4e3cb1ff2508167517f9cde1c99df4e461aff4e
Okan Emre KOCA <oknakoca@gmail.com>
2024-06-18 08:18:49 +0200

    trying to get learn threads for now

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 07:21:25 by okoca             #+#    #+#             */
-/*   Updated: 2024/06/18 07:40:47 by okoca            ###   ########.fr       */
+/*   Updated: 2024/06/18 08:16:58 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 void	*task(void *arg)
 {
+	(void)arg;
 	printf("arg from inside thread number: %d\n", *((int *)arg));
+	// printf("hello world!!\n");
 	return (NULL);
 }
 
@@ -22,16 +24,16 @@ void	pl_create_threads(int max)
 {
 	int			i;
 	pthread_t	*f_thread;
+	int			p_r;
 
 	i = 0;
 	f_thread = malloc(sizeof(pthread_t) * max);
 	while (i < max)
 	{
-		f_thread[i] = pthread_create(&(f_thread[i]), NULL, &task, &i);
-		if (f_thread[i] != 0)
+		if (pthread_create(f_thread + i, NULL, task, &i))
 		{
-			free(f_thread);
 			printf("error occured when creating threads, exiting..\n");
+			free(f_thread);
 			return ;
 		}
 		i++;
@@ -39,10 +41,12 @@ void	pl_create_threads(int max)
 	i = 0;
 	while (i < max)
 	{
-		if (pthread_join(f_thread[i], NULL) != 0)
+		p_r = pthread_join(f_thread[i], NULL);
+		if (p_r != 0)
 		{
+			printf("error occured when creating threads, exiting..\n");
 			free(f_thread);
-			printf("join: error, exiting..");
+			return ;
 		}
 		i++;
 	}
@@ -55,6 +59,7 @@ int main(int ac, char **av)
 	int	i;
 	int	max;
 
+	printf("str1: %s, av[1]: %d\n", av[1], ft_atoi(av[1]));
 	if (ac != 2)
 	{
 		printf("womp womp\n");
