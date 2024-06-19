d1ddce9394db660a078c9f4a8aabeb57764307fc
Okan Emre KOCA <oknakoca@gmail.com>
2024-06-19 12:25:42 +0200

    check to see if philo died

--- philo/src/action.c
+++ philo/src/action.c
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 09:44:28 by okoca             #+#    #+#             */
-/*   Updated: 2024/06/19 12:03:51 by okoca            ###   ########.fr       */
+/*   Updated: 2024/06/19 12:21:17 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,20 @@ void	*pl_action(void *arg)
 	philo = (t_philo *)arg;
 	while (1)
 	{
+		// pthread_mutex_lock(&(philo->data->log_mutex));
+		if (pl_get_time() - philo->last_eaten > philo->data->time_to_die)
+		{
+			pl_log_action(philo, DIED);
+			// pthread_mutex_unlock(&(philo->data->log_mutex));
+			break ;
+		}
+		// pthread_mutex_unlock(&(philo->data->log_mutex));
+		pl_eating(philo);
 		pl_eating(philo);
 		pl_sleep(philo);
 		pl_log_action(philo, THINK);
 	}
+	printf("hello\n\n");
 	return (NULL);
 }
 
@@ -46,6 +56,8 @@ int	pl_log_action(t_philo *philo, t_action action)
 		printf("%lld %d is thinking\n", pl_get_time() - philo->data->start_time, philo->id);
 	else if (action == EAT)
 		printf("%lld %d is eating\n", pl_get_time() - philo->data->start_time, philo->id);
+	else if (action == DIED)
+		printf("%lld %d has died\n", pl_get_time() - philo->data->start_time, philo->id);
 	pthread_mutex_unlock(&(philo->data->log_mutex));
 	return (0);
 }
