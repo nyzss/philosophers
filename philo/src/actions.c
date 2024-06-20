bde03a47f982c24b549b6a78463d9c8f2efd7792
Okan Emre KOCA <oknakoca@gmail.com>
2024-06-20 10:49:37 +0200

    logging is implemented in actions

--- philo/src/actions.c
+++ philo/src/actions.c
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 08:57:01 by okoca             #+#    #+#             */
-/*   Updated: 2024/06/20 10:28:45 by okoca            ###   ########.fr       */
+/*   Updated: 2024/06/20 10:47:08 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ int		pl_lock_fork_mutexes(t_philo *philo)
 	if (philo->id % 2 == 0)
 	{
 		pthread_mutex_lock(philo->left_fork);
-		printf("%lld - %d hold fork!\n", pl_get_time() - philo->data->start_time, philo->id);
+		pl_log(philo, FORK);
 		pthread_mutex_lock(philo->right_fork);
-		printf("%lld - %d hold fork!\n", pl_get_time() - philo->data->start_time, philo->id);
+		pl_log(philo, FORK);
 	}
 	else
 	{
 		pthread_mutex_lock(philo->right_fork);
-		printf("%lld - %d hold fork!\n", pl_get_time() - philo->data->start_time, philo->id);
+		pl_log(philo, FORK);
 		pthread_mutex_lock(philo->left_fork);
-		printf("%lld - %d hold fork!\n", pl_get_time() - philo->data->start_time, philo->id);
+		pl_log(philo, FORK);
 	}
 	return (0);
 }
@@ -43,10 +43,8 @@ int		pl_unlock_fork_mutexes(t_philo *philo)
 	else
 	{
 		pthread_mutex_unlock(philo->right_fork);
-		printf("%lld - %d dropped fork!\n", pl_get_time() - philo->data->start_time, philo->id);
 		pl_log(philo, DROP);
 		pthread_mutex_unlock(philo->left_fork);
-		printf("%lld - %d dropped fork!\n", pl_get_time() - philo->data->start_time, philo->id);
 		pl_log(philo, DROP);
 	}
 	return (0);
--- philo/src/start.c
+++ philo/src/start.c
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 15:52:05 by okoca             #+#    #+#             */
-/*   Updated: 2024/06/20 10:30:05 by okoca            ###   ########.fr       */
+/*   Updated: 2024/06/20 10:33:56 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,12 @@ int	pl_start_philos(t_data *data)
 
 int	pl_check_dead(t_data *data, t_philo *philo, int *finished)
 {
+	// printf("out meal remaining: %d\n", philo->meal_remaining);
 	if (philo->last_eaten > data->time_to_die)
 		data->should_end = 1;
 	if (philo->meal_remaining == 0)
 	{
-		printf("meal remaining: %d\n", philo->meal_remaining);
+		// printf("meal remaining: %d\n", philo->meal_remaining);
 		*finished += 1;
 	}
 	return (0);
@@ -46,8 +47,8 @@ int	pl_track_philos( t_data *data, t_philo *philos)
 		finished = 0;
 		j = 0;
 		pthread_mutex_lock(&(data->log_mutex));
-		printf("data: %d\n", data->should_end);
-		printf("max meal: %d\n", data->maximum_meal);
+		// printf("data: %d\n", data->should_end);
+		// printf("max meal: %d\n", data->maximum_meal);
 		while (j < data->nb_philo)
 		{
 			// printf("%lld -> %d - last eaten: %lld\n", pl_get_time() - data->start_time, philos[j].id,  pl_get_time() - philos[j].last_eaten);
