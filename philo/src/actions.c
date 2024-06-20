523f47254d88f1374530dd61891c3bd45eab63ae
Okan Emre KOCA <oknakoca@gmail.com>
2024-06-20 11:55:15 +0200

    logging and mutex locking handled better

--- philo/src/actions.c
+++ philo/src/actions.c
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 08:57:01 by okoca             #+#    #+#             */
-/*   Updated: 2024/06/20 10:47:08 by okoca            ###   ########.fr       */
+/*   Updated: 2024/06/20 11:47:29 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,10 +52,12 @@ int		pl_unlock_fork_mutexes(t_philo *philo)
 int		pl_eat_action(t_philo *philo)
 {
 	pl_lock_fork_mutexes(philo);
+	pl_log(philo, EAT);
+	pthread_mutex_lock(&(philo->data->log_mutex));
 	philo->last_eaten = pl_get_time();
 	philo->meal_remaining -= 1;
-	pl_log(philo, EAT);
 	usleep(1000 * philo->data->time_to_eat);
+	pthread_mutex_unlock(&(philo->data->log_mutex));
 	pl_unlock_fork_mutexes(philo);
 	return (0);
 }
