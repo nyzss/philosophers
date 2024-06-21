a73f50c5fc7f50e10ee859dcc5eda8e9314ae2af
Okan Emre KOCA <oknakoca@gmail.com>
2024-06-21 16:06:14 +0200

    minor change in order of locking mutex

--- philo/src/actions.c
+++ philo/src/actions.c
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 08:57:01 by okoca             #+#    #+#             */
-/*   Updated: 2024/06/21 15:58:10 by okoca            ###   ########.fr       */
+/*   Updated: 2024/06/21 16:03:41 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,7 +89,6 @@ void	*pl_action(void *arg)
 	philo = (t_philo *)arg;
 	if (philo->data->nb_philo == 1)
 	{
-		// pl_eat_action(philo);
 		pl_log(philo, FORK);
 		usleep(1000 * philo->data->time_to_die);
 		pl_log(philo, DIED);
@@ -100,10 +99,10 @@ void	*pl_action(void *arg)
 		if (pl_end_check(philo) == 1 || philo->meal_remaining == 0)
 			break ;
 		pl_eat_action(philo);
-		if (pl_end_check(philo) == 1)
+		if (pl_end_check(philo) == 1 || philo->meal_remaining == 0)
 			break ;
 		pl_sleep_action(philo);
-		if (pl_end_check(philo) == 1)
+		if (pl_end_check(philo) == 1 || philo->meal_remaining == 0)
 			break ;
 		pl_think_action(philo);
 	}
--- philo/src/logging.c
+++ philo/src/logging.c
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 10:23:17 by okoca             #+#    #+#             */
-/*   Updated: 2024/06/21 15:55:01 by okoca            ###   ########.fr       */
+/*   Updated: 2024/06/21 16:05:38 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int	pl_log(t_philo *philo, t_action action)
 {
+	pthread_mutex_lock(&(philo->data->log_mutex));
 	if (action == DIED)
 	{
 		printf("%lld %d has died\n", pl_get_time() - philo->data->start_time, philo->id);
 		return (0);
 	}
-	pthread_mutex_lock(&(philo->data->log_mutex));
 	pthread_mutex_lock(&(philo->data->meal_mutex));
 	if (philo->data->maximum_meal == 0)
 		return (0);
