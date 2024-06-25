f9f1705c283d77d7b5e4053e808be1da36dd4959
Okan Emre KOCA <oknakoca@gmail.com>
2024-06-25 12:57:57 +0200

    norme

--- philo/src/forks.c
+++ philo/src/forks.c
@@ -6,11 +6,36 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 18:02:59 by okoca             #+#    #+#             */
-/*   Updated: 2024/06/24 09:38:26 by okoca            ###   ########.fr       */
+/*   Updated: 2024/06/25 12:57:33 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+int	pl_lock_second_part(t_philo *philo)
+{
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(philo->right_fork);
+		if (philo->data->should_end == 1)
+		{
+			pl_unlock_fork_mutexes(philo);
+			return (1);
+		}
+		pl_log(philo, FORK);
+	}
+	else
+	{
+		pthread_mutex_lock(philo->left_fork);
+		if (philo->data->should_end == 1)
+		{
+			pl_unlock_fork_mutexes(philo);
+			return (1);
+		}
+		pl_log(philo, FORK);
+	}
+	return (0);
+}
 
 int	pl_lock_fork_mutexes(t_philo *philo)
 {
@@ -20,7 +45,7 @@ int	pl_lock_fork_mutexes(t_philo *philo)
 		if (philo->data->should_end == 1)
 		{
 			pthread_mutex_unlock(philo->left_fork);
-			return (0);
+			return (1);
 		}
 		pl_log(philo, FORK);
 	}
@@ -30,30 +55,11 @@ int	pl_lock_fork_mutexes(t_philo *philo)
 		if (philo->data->should_end == 1)
 		{
 			pthread_mutex_unlock(philo->right_fork);
-			return (0);
+			return (1);
 		}
 		pl_log(philo, FORK);
 	}
-	if (philo->id % 2 == 0)
-	{
-		pthread_mutex_lock(philo->right_fork);
-		if (philo->data->should_end == 1)
-		{
-			pl_unlock_fork_mutexes(philo);
-			return (0);
-		}
-		pl_log(philo, FORK);
-	}
-	else
-	{
-		pthread_mutex_lock(philo->left_fork);
-		if (philo->data->should_end == 1)
-		{
-			pl_unlock_fork_mutexes(philo);
-			return (0);
-		}
-		pl_log(philo, FORK);
-	}
+	pl_lock_second_part(philo);
 	return (0);
 }
 
